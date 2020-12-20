e243d798cba601e2cbd7c6b345c018fab9b7f7f0
armen-matevosyan <64553251+armen-matevosyan@users.noreply.github.com>
2020-12-20 18:19:25 +0400

    Add files via upload

---
+++ WidgetController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Controllers
+{
+    [Route("Widget")]
+    public class WidgetController : Controller
+    {
+       
+        public IActionResult Index([FromQuery] string key)
+        {
+            ViewBag.key = key;
+
+            return View();
+        }
+
+    }
+}
+
